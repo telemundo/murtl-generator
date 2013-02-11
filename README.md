9f240f6f523ca485cf5be3f749bb2bdc64b84cee
Rodolfo Puig <rpuig@7gstudios.com>
2013-02-11 18:56:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MURTL Injector
+
+MURTL Injector is a tool to create rules in [MURTL](http://murtl.nbcudps.com/) from flat files.
+
+## Authors
+
+Rodolfo Puig [@rudisimo](https://twitter.com/rudisimo)
+
+## License
+
+This package is licensed under the MIT License - see the LICENSE file for details.
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the MURTL package.
+ *
+ * (c) Telemundo Digital Media
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once 'vendor/autoload.php';
+
+$app = new Silex\Application;
+$app->run();
